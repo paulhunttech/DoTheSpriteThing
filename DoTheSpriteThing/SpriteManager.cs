4d5a71a148c40e1e7e198f0923197abecddcb4e8
Paul Hunt <woodburysoft@gmail.com>
2017-02-02 20:19:58 +0000

    Added noImageFilename parameter to CreateSprite. Set version to 2.0.0.

--- DoTheSpriteThing/SpriteManager.cs
+++ DoTheSpriteThing/SpriteManager.cs
@@ -35,20 +35,42 @@ namespace DoTheSpriteThing
         /// Create a sprite image from a list of images and the CSS to render each image.
         /// </summary>
         /// <param name="images">The list of images to include in the sprite. The key is the ID of the HTML element in which to display the image and the value is the image data.</param>
+        /// <param name="noImageFilename">The name of the image file to use when the image data is null.</param>
         /// <param name="spriteSettings">The settings to use when creating the sprite.</param>            
-        public void CreateSprite(IReadOnlyDictionary<string, byte[]> images, SpriteSettings spriteSettings)
+        public void CreateSprite(IReadOnlyDictionary<string, byte[]> images, string noImageFilename, SpriteSettings spriteSettings)
         {
+            var noImageRequired = false;
+            var noImageTop = 0;
+
             using (var imageCollection = new MagickImageCollection())
             {
                 var css = new StringBuilder();
                 var imageTop = 0;
 
-                foreach (KeyValuePair<string, byte[]> image in images.Where(x => x.Value != null))
+                foreach (KeyValuePair<string, byte[]> image in images)
                 {
-                    imageCollection.Add(new MagickImage(image.Value));
-                    css.AppendLine(GetCss(image.Key, spriteSettings.ImageHeight, spriteSettings.ImageWidth, spriteSettings.SpriteUrl, imageTop));
+                    int useImageTop;
+
+                    if (image.Value != null)
+                    {
+                        imageCollection.Add(new MagickImage(image.Value));
+                        useImageTop = imageTop;
+                    }
+                    else if (!noImageRequired)
+                    {
+                        noImageRequired = true;
+                        imageCollection.Add(new MagickImage(new FileInfo(noImageFilename)));                                                
+                        noImageTop = imageTop;
+                        useImageTop = imageTop;
+                    }
+                    else
+                    {
+                        useImageTop = noImageTop;
+                    }
+                    
+                    css.AppendLine(GetCss(image.Key, spriteSettings.ImageHeight, spriteSettings.ImageWidth, spriteSettings.SpriteUrl, useImageTop));
                     imageTop += spriteSettings.ImageHeight;
-                }
+                }                
 
                 GenerateSpriteAndCss(imageCollection, css, spriteSettings.SpriteFilename, spriteSettings.CssFilename, spriteSettings.ImageHeight, spriteSettings.ImageWidth);
             }
