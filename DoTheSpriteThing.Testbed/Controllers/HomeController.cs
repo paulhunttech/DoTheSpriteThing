975efea9860294eea77ca9fe7b5ad8d549aa32af
Paul Hunt <woodburysoft@gmail.com>
2017-02-14 14:38:58 +0000

    Added rendering of images from a folder to the testbed site

--- DoTheSpriteThing.Testbed/Controllers/HomeController.cs
+++ DoTheSpriteThing.Testbed/Controllers/HomeController.cs
@@ -1,35 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DoTheSpriteThing.Testbed.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         public IActionResult Index()
         {
+            var spriteManager = new SpriteManager();
+            string imagesFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+            string spriteFolder = Path.Combine(_hostingEnvironment.WebRootPath, @"images\sprites");
+
+            if (!Directory.Exists(spriteFolder))
+            {
+                Directory.CreateDirectory(spriteFolder);
+            }
+
+            string spriteFilename = Path.Combine(spriteFolder, "sprite.png");
+            const string spriteUrl = "../images/sprites/sprite.png";
+            string cssFilename = Path.Combine(_hostingEnvironment.WebRootPath, @"css\sprite.css");            
+
+            spriteManager.CreateSprite(imagesFolder, new SpriteSettings(spriteFilename, spriteUrl, cssFilename));            
+
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-        }
+        }                
     }
 }
