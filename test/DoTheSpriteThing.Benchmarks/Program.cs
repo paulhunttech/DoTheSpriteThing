ba1a771503a0c68f25fcb5a1c35a22cdfeacf628
Paul Hunt <woodburysoft@gmail.com>
2017-02-24 12:16:15 +0000

    Added benchmarks project

---
+++ test/DoTheSpriteThing.Benchmarks/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using DoTheSpriteThing.Images;
+
+namespace DoTheSpriteThing.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var images = new List<FileSpriteImage>();            
+            string[] imageFilenames = Directory.GetFiles(@"..\DoTheSpriteThing.Testbed\wwwroot\images");            
+
+            foreach (string imageFilename in imageFilenames)
+            {
+                images.Add(new FileSpriteImage(new FileInfo(imageFilename)));
+            }
+
+            var spriteManager = new SpriteManager();
+            var runTimes = new List<long>();
+
+            for (var i = 0; i < 20; i++)
+            {
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                spriteManager.CreateSprite(images, new SpriteSettings("sprite.png", "/sprite.png", "sprite.css"));
+                stopwatch.Stop();
+                runTimes.Add(stopwatch.ElapsedMilliseconds);
+                Console.WriteLine($"Execution {i} time: {stopwatch.ElapsedMilliseconds}ms");
+            }  
+            
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Finished");
+            Console.WriteLine($"Total execution time: {runTimes.Sum()}ms");
+            Console.WriteLine($"Average execution time: {runTimes.Average()}ms");
+            Console.WriteLine("--------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
